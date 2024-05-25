4ba130f1bc2f05ef93c7fe41e6f037f7882da551
Serge <s3rg3y@hotmail.com>
2024-05-26 03:50:56 +0930

    gui

--- TimesTables.m
+++ TimesTables.m
@@ -1,30 +1,84 @@
-function TimesTables(N1, N2, OP, m)
+function TimesTables
 
-% Defaults
+% Figure
+close(findobj(0, 'Name', 'Times Tables')) % For convenience only
+fig = figure(...
+    Position = get(0).ScreenSize([3 4 3 4]).*[0.2 0.1 0.6 0.8], ...
+    Name = 'Times Tables', NumberTitle = 'off', ...
+    ToolBar = 'none', MenuBar = 'none', ...
+    DefaultUipanelUnits = 'normalized', ...
+    DefaultUicontrolUnits = 'normalized', ...
+    DefaultTextboxshapeUnits = 'normalized', ...
+    DefaultUicontrolFontSize = 14, ...
+    DefaultTextboxshapeFontSize = 14, ...
+    DefaultUicontrolFontWeight = 'bold', ...
+    DefaultTextboxshapeEdgeColor = 'n', ...
+    DefaultTextboxshapeInterpreter = 'n', ...
+    DefaultTextboxshapeVerticalAlignment = 'middle', ...
+    DefaultTextboxshapeHorizontalAlignment = 'cen');
+
+% Setting
 N1 = 2:11;   % Numbers
 N2 = 2:11;   % Numbers
 OP = ["times" "divide"]; % Opperators
-num_questions = 50;
 dynamic = true;
+p1 = uipanel(fig, 'OuterPosition', [0 0.75 0.5 0.25], 'BorderType', 'beveledin');
+annotation(p1, 'textbox', 'Position', [0.0 0.7 0.25 0.2], 'String', 'Numbers');
+annotation(p1, 'textbox', 'Position', [0.0 0.4 0.25 0.2], 'String', 'Numbers');
+annotation(p1, 'textbox', 'Position', [0.0 0.1 0.25 0.2], 'String', 'Operators');
+N1h = uicontrol (p1, 'Style', 'edit', 'Position', [0.25 0.7 0.7 0.2], 'String', strjoin(string(N1)));
+N2h = uicontrol (p1, 'Style', 'edit', 'Position', [0.25 0.4 0.7 0.2], 'String', strjoin(string(N2)));
+O1h = uicontrol (p1, 'Style', 'chec', 'Position', [0.35 0.1 0.3 0.2], 'String', 'Times', 'Value', 1);
+O2h = uicontrol (p1, 'Style', 'chec', 'Position', [0.65 0.1 0.3 0.2], 'String', 'Divide', 'Value', 1);
+
+% Assessment
+p2 = uipanel(fig, 'OuterPosition', [0.5 0.75 0.5 0.25], 'BorderType', 'beveledin');
+annotation(p2, 'textbox', 'Position', [0.0 0.7 0.3 0.2], 'String', 'Max Time');
+annotation(p2, 'textbox', 'Position', [0.0 0.4 0.3 0.2], 'String', 'Max History');
+annotation(p2, 'textbox', 'Position', [0.0 0.1 0.3 0.2], 'String', 'Gamma');
+max_time_h = uicontrol(p2, 'Style', 'edit', 'Position', [0.25 0.7 0.7 0.2], 'String', 20);
+max_hist_h = uicontrol(p2, 'Style', 'edit', 'Position', [0.25 0.4 0.7 0.2], 'String', 5);
+gamma_h    = uicontrol(p2, 'Style', 'edit', 'Position', [0.25 0.1 0.7 0.2], 'String', 1);
+
+% Results
+p3 = uipanel(fig, 'OuterPosition', [0.5 0.0 0.5 0.75], 'BorderType', 'beveledin');
+
+num_questions = 50;
 max_time = 20; % Maximum time per question
 max_hist = 5;  % Look at only this many previous results
 gamma = 1;
 
-% Checks
-cd(fileparts(mfilename('fullpath')))
+T = read_logs(dir('log/*.log'));
+plotResults(p3, N1, N2, OP, max_time, max_hist, gamma, T)
+
+% Answer box
+hAns = uicontrol('Style', 'edit', 'Units', 'normalized', 'Position', [0.05 0.4 0.4 0.1], 'FontSize', 14, 'HorizontalAlignment', 'right');
+
+% Number pad
+hPanel = uipanel(fig, 'Units', 'normalized', 'Position', [0.05 0.05 0.4 0.3]);
+t = {'7' '8' '9'; '4' '5' '6'; '1' '2' '3'; '<' '0' 'Enter'};
+for r = 1:4
+    for c = 1:3
+        if ~isempty(t{r, c})
+            h(r, c) = uicontrol(hPanel, 'Style', 'pushbutton', 'String', t{r, c}, ...
+                'Units', 'normalized', 'Position', [c/3-1/3 1-r/4 1/3 1/4], ...
+                'Callback', @(obj, ~)buttonCallback(obj.String, hAns));
+        end
+    end
+end
+
+
 
 % Generate nubers
 if dynamic
     % Read previous results
+    num_questions = 50;
+    max_time = 20; % Maximum time per question
+    max_hist = 5;  % Look at only this many previous results
+    gamma = 1;
     T = read_logs(dir('log/*.log'));
-
-    % Give each question a probability based on past results
-    if isempty(T)
-        p = ones(numel(N1), numel(N2));
-    else
-        for k = numel(OP) : -1 : 1
-            [~, ~, p(:,:,k)] = calc_stats(T, OP(k), N1, N2, max_time, max_hist, gamma);
-        end
+    for k = numel(OP) : -1 : 1
+        [~, ~, p(:,:,k)] = calc_stats(T, OP(k),  N1, N2, max_time, max_hist, gamma);
     end
 
     % Pick questions
@@ -47,11 +101,12 @@ end
 if ~isfolder('log')
     mkdir('log')
 end
-file = sprintf('log/%s_[%s]_[%s]_[%s]_%g.log', datetime('now', 'Format', 'yyyyMMdd_HHmm'), strjoin(string(N1)), strjoin(string(N2)), strjoin(OP), numel(V1));
-%eg log/20240525_2225_[2 3 4 5 6 7 8 9 10 11]_[2 3 4 5 6 7 8 9 10 11]_[times divide]_50.log
+file = sprintf('log/%s_[%s]_[%s]_%s_%g.log', ...
+    string(datetime('now', 'Format', 'yyyyMMdd_HHmm')), ...
+    strjoin(string(N1)), strjoin(string(N2)), strjoin(OP), numel(V1));
 pause(1)
 
-% Step through questions
+% Loop through number of questions
 for k = 1 : numel(V1)
 
     % Generate question
@@ -100,9 +155,9 @@ for k = 1 : numel(V1)
     pause(1)
 
     % Log to file
-    log(file, '%3d, %3d, %6s, %3d, %3d, %1d, %4.1f', num1, num2, opp, answer, reply, reply==answer, t);
+    log(file, '%3d, %3d, %6s, %3d, %3d, %1d, %4.1f', num1, num2, opp, answer, reply, reply==answer, toc);
 
-    TimesTablesResults(N1, N2, OP, max_time, max_hist, gamma, T)
+    % TimesTablesResults(N1, N2, OP, max_time, max_hist, gamma, T)
 end
 end
 
@@ -139,4 +194,91 @@ fid = fopen(file, 'a');
 str = sprintf(varargin{:});
 fprintf(fid, '%s\n', str);
 fclose(fid);
+end
+
+function buttonCallback(str, h)
+if isfinite(str2double(str))
+    h.String = [h.String str];
+elseif str == "<"
+    h.String = h.String(1:end-1);
+else
+    disp Enter
+end
+end
+
+function plotResults(h, N1, N2, OP, max_time, max_count, gamma, T)
+% Step through operators
+for k = 1:numel(OP)
+    [Time, Count] = calc_stats(T, OP(k), N1, N2, max_time, max_count, gamma);
+    
+    % Plot stats
+    axes(h, 'Position', [0.05 (k-1)/numel(OP)+0.01 0.9 0.4]);
+    title(sprintf('%s\navg = %.2f sec', OP(k), mean(Time(Time>0))))
+    plotMatrix(N1, N2, Count, round(Time), Count/max_count)
+    
+    col = interp1(0:1, [0 1 0; 1 0 0], linspace(0, 1, 64)).^(1/2.4);
+    colormap([0.8 0.8 0.8; col])
+    clim([0 max_time])
+    colorbar
+end
+end
+
+function plotMatrix(X, Y, Text, Color, Alpha)
+% Display a matrix as a grid of cells, with text, color and alpha.
+hold on, axis equal tight % Prepre axis
+surf(0:numel(X), 0:numel(Y), zeros(numel(Y) + 1, numel(X) + 1), ... % Display matrix
+    'CData', Color', 'FaceColor', 'flat','EdgeColor', 'w', 'LineWidth', 2,... % Color
+    'FaceAlpha', 'flat', 'AlphaData', Alpha', 'AlphaDataMapping', 'none') % Alpha
+set(gca, 'XTick', 0.5:numel(X), 'XTickLabel', X) % Set x tick marks
+set(gca, 'YTick', 0.5:numel(Y), 'YTickLabel', Y) % Set y tick marks
+set(gca, 'XAxisLocation', 'top', 'YDir', 'reverse') % Change axis location
+[X, Y] = ndgrid(0.5:numel(X), 0.5:numel(Y)); % Text locations
+text(gca, X(:), Y(:), string(Text), 'HorizontalAlignment', 'center', 'Clipping', 'on'); % Show text
+end
+
+function logs = read_logs(files)
+for k = numel(files) : -1 : 1
+    logs{k} = readtable(fullfile(files(k).folder, files(k).name));
+end
+logs = cat(1, logs{:});
+end
+
+function [Time, Count, Prob] = calc_stats(logs, op, x, y, max_time, max_count, gamma)
+% Filter on operator
+logs = logs(logs.Var3 == op, :);
+
+% FIX
+if op == "divide"
+    logs.Var1 = logs.Var1./logs.Var2;
+end
+
+% Filter old results
+if size(logs, 1) > max_count
+    [~, i] = sortrows([logs.Var1  logs.Var2]);
+    logs = logs(i, :);
+    idx = [logs.Var1  logs.Var2];
+    ind = all(idx(max_count + 1 : end, :) == idx(1 : end - max_count, :), 2);
+    logs(ind, :) = [];
+end
+
+% Main
+X = 1:max(logs.Var1);
+Y = 1:max(logs.Var2);
+sz = [max(X) max(Y)];
+ind = [logs.Var1  logs.Var2];
+time_list = max( min(logs.Var7, max_time), ~logs.Var6 * max_time); % Replace wrong answers with max time
+Time = accumarray(ind, time_list,   sz, @median);
+Count = accumarray(ind, time_list>0, sz, @sum);
+
+% Subset
+xi = ismember(X, x);
+yi = ismember(Y, y);
+Time = Time(xi, yi);
+Count = Count(xi, yi);
+
+% Edge case
+Time(Count==0) = max_time;
+
+% Relative probability (coef)
+Prob = (Time/max_time).^gamma;
 end
